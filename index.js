1cc682fc37f640a149e942c5af9e4a62575d0d2b
Pranav-Khude <202151077@iiitvadodara.ac.in>
2025-01-03 20:30:12 +0530

    added env

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
 
@@ -65,7 +66,7 @@ app.delete('/teas/:id', (req, res) => {
 
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}........`);
